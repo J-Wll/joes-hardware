f6d8e2e63799ac1ffc7715f010fd52eea7941d06
Joe-W <80954812+J-Wll@users.noreply.github.com>
2025-08-24 16:38:14 +0100

    Planning components

---
+++ app/ui/catalogue.tsx
@@ -0,0 +1,7 @@
+export default function Catalogue(){
+    return (
+        <div className="catalogue">
+            <h2 className="text-xl">Catalogue Component Placeholder</h2>
+        </div>
+    )
+}
---
+++ app/ui/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="p-6 bg-gray-100 w-full text-center">
+            <h1 className="h1 text-2xl">{"Joe's Hardware - Internal Catalogue"}</h1>
+        </header>
+    )
+}
